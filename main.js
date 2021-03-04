4079ab08d402b4f34a0bca424b286a6eb4ec29eb
dmytropetrenko <dmitry.a.petrenko@gmail.com>
2021-03-04 10:07:57 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,74 +1,19 @@
 window.onload = function () {
-	var locations = []
-	var squeres = []
-	var guesses = 0
+	let locations = []
+	let squeres = []
+	let guesses = 0
 	isSunk = false
 
-	locations = hideShips()
+	locations = generateCoordinatesForShips()
+	squeres = getSqueres()
+	initBoard(locations, squeres)
 
-	locations.forEach((location) => {
-		console.log(location)
-	})
-
-	squeres = document.getElementsByClassName("squere")
-
-	locations.forEach((location) => {
-		squeres[location].innerHTML = "<p>Ship</p>"
-	})
-
-	if (squeres) {
-		Array.from(squeres).forEach((squere) => {
-			if (squere.firstChild === null) {
-				squere.innerHTML = "<p>X</p>"
-			}
-		})
-	}
-	updateShipLives()
 	for (let i = 0; i < squeres.length; i++) {
 		const squere = squeres[i]
-		squere.onclick = showClicked
+		squere.onclick = updateOnSquereClick
 	}
 
-	function shipSunked() {
-		let numDel = -1
-		let squereNum = this.id
-		for (let i = 0; i < locations.length; i++) {
-			const loc = locations[i]
-			if (loc == squereNum) {
-				numDel = i
-			}
-		}
-		if (numDel != -1) {
-			locations.splice(numDel, 1)
-			updateShipLives()
-		}
-		updateGuesses()
-	}
-
-	function updateShipLives() {
-		let shipLives = document.getElementById("shipLives")
-		if (locations.length) {
-			shipLives.innerText = locations.length
-		} else {
-			shipLives.innerText = "All shipes sunked! You win the game! Congratulations!"
-		}
-	}
-
-	function showClicked() {
-		if (this.children[0].style.opacity !== 1) {
-			this.children[0].style.opacity = 1
-		} else {
-			this.children[0].style.opacity = 0
-		}
-		shipSunked()
-	}
-
-	function updateGuesses() {
-		guesses++
-		document.getElementById("guesses").innerText = guesses
-	}
-
-	function hideShips() {
+	function generateCoordinatesForShips() {
 		let arr = []
 		let rNum = 0
 		for (let i = 5; i > arr.length; ) {
@@ -79,8 +24,72 @@ window.onload = function () {
 		}
 		return arr
 	}
-
 	function randomNum(max = 48) {
 		return Math.floor(Math.random() * max)
+	}
+	function getSqueres() {
+		if (document.getElementsByClassName("squere")) {
+			return document.getElementsByClassName("squere")
+		}
+		return new Error("No squeres on the board!!!")
+	}
+	function hideShipsOnLocations(locations, squeres) {
+		if (squeres && locations) {
+			locations.forEach((location) => {
+				squeres[location].innerHTML = "<p>Ship</p>"
+			})
+		}
+	}
+	function hideMissesOnBoard(squeres) {
+		if (squeres) {
+			Array.from(squeres).forEach((squere) => {
+				if (squere.firstChild === null) {
+					squere.innerHTML = "<p>X</p>"
+				}
+			})
+		}
+	}
+	function initBoard(locations, squeres) {
+		hideShipsOnLocations(locations, squeres)
+		hideMissesOnBoard(squeres)
+		updateShipLives()
+	}
+	function showClicked(elem) {
+		if (elem.children[0].style.opacity !== 1) {
+			elem.children[0].style.opacity = 1
+		} else {
+			elem.children[0].style.opacity = 0
+		}
+	}
+	function shipSunked(elem) {
+		let numDel = -1
+		let squereNum = elem.id
+		for (let i = 0; i < locations.length; i++) {
+			const loc = locations[i]
+			if (loc == squereNum) {
+				numDel = i
+			}
+		}
+		if (numDel != -1) {
+			locations.splice(numDel, 1)
+			updateShipLives()
+		}
+	}
+	function updateShipLives() {
+		let shipLives = document.getElementById("shipLives")
+		if (locations.length) {
+			shipLives.innerText = locations.length
+		} else {
+			shipLives.innerText = "All shipes sunked! You win the game! Congratulations!"
+		}
+	}
+	function updateGuesses() {
+		guesses++
+		document.getElementById("guesses").innerText = guesses
+	}
+	function updateOnSquereClick() {
+		showClicked(this)
+		shipSunked(this)
+		updateGuesses()
 	}
 }
